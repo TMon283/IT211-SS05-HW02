b617e1ec991bac2e80a4fda6ee5964e12e5e2680
TMon283 <lemonboy2k6@gmail.com>
2026-05-21 00:29:00 +0700

    up

---
+++ src/main/java/com/example/it211ss05hw02/It211Ss05Hw02Application.java
@@ -0,0 +1,13 @@
+package com.example.it211ss05hw02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It211Ss05Hw02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(It211Ss05Hw02Application.class, args);
+    }
+
+}
